67e0b5fb887c73f4283d2b3f4bfc5918e816891c
Hoang Hung <hungnh219@gmail.com>
2024-07-25 23:56:13 +0700

    [add] task file

--- lib/presentation/screens/splash/splash_page.dart
+++ lib/presentation/screens/splash/splash_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sudoku/presentation/screens/game/game_page.dart';
 
+// task: auto navigate after 5s
+
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
@@ -20,7 +22,9 @@ class SplashPage extends StatelessWidget {
               ElevatedButton(onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => GamePage())
+                  MaterialPageRoute(builder: (context) {
+                    return GamePage();
+                  })
                 );
               }, child: Text('Next'))
             ]
